acf236cb22fdf311a2cc9899bb83a98d901a7041
Manuel_Sevilla <manuelsevilla@unah.hn>
2021-11-13 21:30:49 -0600

    se creo un archivo de funciones en R

---
+++ Funciones.R
@@ -0,0 +1,11 @@
+#Trabajando con funciones
+#function(){}
+#los objetos X y Y son variables arbitrarias 
+#dentro de los corchetes van las instrucciones de la funcion
+
+suma <-function(x,y){
+  adicion <- x+y
+  resta <- x-y
+  print(adicion)
+  print(resta)
+}
